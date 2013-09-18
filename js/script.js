dc54d4fa68acc87391b6d099bab3d9ee3511edab
jameswoods <gurufoo@gmail.com>
2013-09-18 12:50:57 -0700

    Adding the mobile menu css/js and updating the reflow widths.

--- js/script.js
+++ js/script.js
@@ -12,8 +12,29 @@
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($, Drupal, window, document, undefined) {
 
+// This toggles the menu checkboxes which are used in the mobile display/layout
+Drupal.behaviors.mobileMenuVisibility = {
+  attach: function (context, settings) {
+    var allMenuCheckBoxes = $('.menus input[type="checkbox"]');
+    allMenuCheckBoxes.change(function(){
+      console.log('checkbox', this);
+      if($(this).is(':checked')) {
+        $(allMenuCheckBoxes).attr('checked', false);
+        $(this).attr('checked', true);
+      }
+    });
+  },
+  detach: function (context, settings) { }
+};
 
-// Place your code here.
 
+// This modifies the text format display to help prevent data loss when
+// switching between text formats
+Drupal.behaviors.textFormatHack = {
+  attach: function (context, settings) {
+    $(".filter-guidelines .tips").addClass('bobo');
+  },
+  detach: function (context, settings) { }
+};
 
 })(jQuery, Drupal, this, this.document);
