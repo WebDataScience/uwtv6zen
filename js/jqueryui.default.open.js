ae21c6523770d985aa1965897e185a44b27fa9ac
James Woods <jamesini@uw.edu>
2014-02-03 13:10:36 -0800

    Modified the behavior of jQueryUI accordion widgets.  I added the option of 'autoscroll' so that the clicked-on accordion header will automatically scroll to the top of the browser window.

--- js/jqueryui.default.open.js
+++ js/jqueryui.default.open.js
@@ -2,11 +2,22 @@ jQuery(document).ready(function ($) {
 
    
     var defaultPanel = parseInt(getParam('panel'));
+    // Accordion with auto scroll
+    jQuery(".accordion-container.autoscroll").accordion({
+        autoHeight:false,
+        collapsible:true,
+        active:defaultPanel,
+        change: function( event, ui){ // Autoscroll callback
+          _scrollToElement(event, ui);
+        }
+      });
+    // Accordion with NO auto scroll
+
     jQuery(".accordion-container").accordion({
         autoHeight:false,
         collapsible:true,
         active:defaultPanel
-        });
+      });
 
     var defaultTab = parseInt(getParam('tab'));
     jQuery(".tab-container").tabs({
@@ -31,4 +42,14 @@ jQuery(document).ready(function ($) {
           return -1;
         }
     }
+
+// Helper callback that moves scrolls the window to the clicked on accordion
+//  container.
+function _scrollToElement(event, ui) {
+  if(jQuery(ui.newHeader).offset()){
+    jQuery('html, body').animate({
+      scrollTop: jQuery(ui.newHeader).offset().top
+      }, 1000);
+    }
+  }
 });
